6d825eb88a809b43f36194c6bbde44be893fca87
Rick Winfrey <rick.winfrey@gmail.com>
2015-11-05 11:03:10 -0600

    Updates specs for ActionTask to cover all cases

--- spec/fixtures/validations.rb
+++ spec/fixtures/validations.rb
@@ -7,4 +7,22 @@ class Validations
                       :array_test   => { :type => :array, :presence => true },
                       :symbol_test  => { :type => :symbol, :presence => true },
                       :nil_test     => { :type => :nil } }
+
+  INVALID_ATTRIBUTES = { :integer_test => nil,
+                         :float_test   => nil,
+                         :string_test  => nil,
+                         :bool_test    => nil,
+                         :hash_test    => nil,
+                         :array_test   => nil,
+                         :symbol_test  => nil,
+                         :nil_test     => 1 }
+
+  VALID_ATTRIBUTES = { :integer_test => 1,
+                       :float_test => 1.0,
+                       :string_test => "string",
+                       :bool_test => true,
+                       :hash_test => {},
+                       :array_test => [],
+                       :symbol_test => :symbol,
+                       :nil_test => nil }
 end
