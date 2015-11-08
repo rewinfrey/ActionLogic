e143aac6b0828a0e530b46f12dd4aee4104aed5b
Rick Winfrey <rick.winfrey@gmail.com>
2015-11-07 22:04:31 -0600

    Adds initial tests and behavior for ActionCoordinator

--- lib/action_logic.rb
+++ lib/action_logic.rb
@@ -1,5 +1,6 @@
 require 'action_logic/version'
 require 'action_logic/action_context'
+require 'action_logic/action_coordinator'
 require 'action_logic/action_core'
 require 'action_logic/action_task'
 require 'action_logic/action_use_case'
---
+++ spec/fixtures/coordinators.rb
@@ -0,0 +1,254 @@
+require 'action_logic'
+require 'fixtures/constants'
+
+class TestCoordinator1
+  extend ActionLogic::ActionCoordinator
+
+  def call(context)
+    context.test_coordinator1 = true
+  end
+
+  def plan
+    {
+      TestUseCase1 => { :success => TestUseCase2 },
+
+      TestUseCase2 => { :success => TestUseCase3 },
+
+      TestUseCase3 => { :success => nil }
+    }
+  end
+end
+
+class HaltedTestCoordinator1
+  extend ActionLogic::ActionCoordinator
+
+  def call(context)
+    context.halted_test_coordinator1 = true
+  end
+
+  def plan
+    {
+      HaltedTestUseCase1 => { :halted  => TestUseCase2 },
+
+      TestUseCase2 => { :success => TestUseCase3 },
+
+      TestUseCase3 => { :success => nil }
+    }
+  end
+end
+
+class FailureTestCoordinator1
+  extend ActionLogic::ActionCoordinator
+
+  def call(context)
+    context.failure_test_coordinator1 = true
+  end
+
+  def plan
+    {
+      FailureTestUseCase1 => { :failure  => TestUseCase2 },
+
+      TestUseCase2 => { :success => TestUseCase3 },
+
+      TestUseCase3 => { :success => nil }
+    }
+  end
+end
+
+class TestUseCase1
+  extend ActionLogic::ActionUseCase
+
+  def call(context)
+    context.test_use_case1 = true
+  end
+
+  def tasks
+    [TestTask1]
+  end
+end
+
+class TestHaltUseCase1
+  extend ActionLogic::ActionUseCase
+
+  def call(context)
+    context.test_use_case1 = true
+  end
+
+  def tasks
+    [HaltTestTask1]
+  end
+end
+
+class TestUseCase2
+  extend ActionLogic::ActionUseCase
+
+  def call(context)
+    context.test_use_case2 = true
+  end
+
+  def tasks
+    [TestTask2]
+  end
+end
+
+class TestUseCase3
+  extend ActionLogic::ActionUseCase
+
+  def call(context)
+    context.test_use_case3 = true
+  end
+
+  def tasks
+    [TestTask3]
+  end
+end
+
+class HaltedTestUseCase1
+  extend ActionLogic::ActionUseCase
+
+  def call(context)
+    context.halted_test_use_case1 = true
+  end
+
+  def tasks
+    [HaltedTestTask1]
+  end
+end
+
+class HaltedTestUseCase2
+  extend ActionLogic::ActionUseCase
+
+  def call(context)
+    context.halted_test_use_case2 = true
+  end
+
+  def tasks
+    [HaltedTestTask2]
+  end
+end
+
+class HaltedTestUseCase3
+  extend ActionLogic::ActionUseCase
+
+  def call(context)
+    context.halted_test_use_case3 = true
+  end
+
+  def tasks
+    [HaltedTestTask3]
+  end
+end
+
+class FailureTestUseCase1
+  extend ActionLogic::ActionUseCase
+
+  def call(context)
+    context.failure_test_use_case1 = true
+  end
+
+  def tasks
+    [FailureTestTask1]
+  end
+end
+
+class FailureTestUseCase2
+  extend ActionLogic::ActionUseCase
+
+  def call(context)
+    context.failure_test_use_case2 = true
+  end
+
+  def tasks
+    [FailureTestTask2]
+  end
+end
+
+class FailureTestUseCase3
+  extend ActionLogic::ActionUseCase
+
+  def call(context)
+    context.failure_test_use_case3 = true
+  end
+
+  def tasks
+    [FailureTestTask3]
+  end
+end
+
+class TestTask1
+  extend ActionLogic::ActionTask
+
+  def call(context)
+    context.test_task1 = true
+  end
+end
+
+class TestTask2
+  extend ActionLogic::ActionTask
+
+  def call(context)
+    context.test_task2 = true
+  end
+end
+
+class TestTask3
+  extend ActionLogic::ActionTask
+
+  def call(context)
+    context.test_task3 = true
+  end
+end
+
+class HaltedTestTask1
+  extend ActionLogic::ActionTask
+
+  def call(context)
+    context.halted_test_task1 = true
+    context.halt!
+  end
+end
+
+class HaltedTestTask2
+  extend ActionLogic::ActionTask
+
+  def call(context)
+    context.halted_test_task2 = true
+    context.halt!
+  end
+end
+
+class HaltedTestTask3
+  extend ActionLogic::ActionTask
+
+  def call(context)
+    context.halted_test_task3 = true
+    context.halt!
+  end
+end
+
+class FailureTestTask1
+  extend ActionLogic::ActionTask
+
+  def call(context)
+    context.failure_test_task1 = true
+    context.fail!
+  end
+end
+
+class FailureTestTask2
+  extend ActionLogic::ActionTask
+
+  def call(context)
+    context.failure_test_task2 = true
+    context.fail!
+  end
+end
+
+class FailureTestTask3
+  extend ActionLogic::ActionTask
+
+  def call(context)
+    context.failure_test_task3 = true
+    context.fail!
+  end
+end
