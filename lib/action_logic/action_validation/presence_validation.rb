d5056e6538d4e0b2e7385df832f68390e96e9237
Rick Winfrey <rewinfrey@github.com>
2016-01-13 21:37:31 -0600

    Splits PresenceValidation logic into helper methods

--- lib/action_logic/action_validation/presence_validation.rb
+++ lib/action_logic/action_validation/presence_validation.rb
@@ -7,22 +7,25 @@ module ActionLogic
 
       def self.validate!(validation_rules, context)
         return unless validation_rules.values.find { |expected_validation| expected_validation[:presence] }
+        errors = presence_errors(validation_rules, context)
+        raise ActionLogic::PresenceError.new(errors) if errors.any?
+      end
 
-        presence_errors = validation_rules.reduce([]) do |collection, (expected_attribute, expected_validation)|
+      def self.presence_errors(validation_rules, context)
+        validation_rules.reduce([]) do |error_collection, (expected_attribute, expected_validation)|
           next unless expected_validation[:presence]
-
-          if expected_validation[:presence] == true
-            collection << "Attribute: #{expected_attribute} is missing value in context but presence validation was specified" unless context[expected_attribute]
-          elsif expected_validation[:presence].class == Proc
-            collection << "Attribute: #{expected_attribute} is missing value in context but custom presence validation was specified" unless expected_validation[:presence].call(context[expected_attribute])
-          else
-            raise ActionLogic::UnrecognizablePresenceValidatorError.new(error_message_format("Presence validator: #{expected_validation[:presence]} is not a supported format"))
-          end
-
-          collection
+          error_collection << error_message(expected_attribute, expected_validation, context)
+          error_collection
         end || []
+      end
 
-        raise ActionLogic::PresenceError.new(presence_errors) if presence_errors.any?
+      def self.error_message(expected_attribute, expected_validation, context)
+        case expected_validation[:presence]
+        when TrueClass then "Attribute: #{expected_attribute} is missing value in context but presence validation was specified" unless context[expected_attribute]
+        when Proc      then "Attribute: #{expected_attribute} is missing value in context but custom presence validation was specified" unless expected_validation[:presence].call(context[expected_attribute])
+        else
+          raise ActionLogic::UnrecognizablePresenceValidatorError.new(error_message_format("Presence validator: #{expected_validation[:presence]} is not a supported format"))
+        end
       end
     end
   end
