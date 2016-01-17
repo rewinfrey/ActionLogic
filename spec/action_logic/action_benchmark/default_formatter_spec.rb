8bda14ad1d5c41fc0e800e245921dca7c33600a3
Rick Winfrey <rewinfrey@github.com>
2016-01-17 15:40:37 -0600

    Updates DefaultFormatter to not include human readable spaces

--- spec/action_logic/action_benchmark/default_formatter_spec.rb
+++ spec/action_logic/action_benchmark/default_formatter_spec.rb
@@ -11,19 +11,19 @@ module ActionLogic::ActionBenchmark
     it "writes the benchmark result to the log for an ActionCoordinator" do
       subject.coordinator(benchmark_result, "CoordinatorContext")
       expect(benchmark_log.string).to\
-        eq "context:   CoordinatorContext                                 user_time: 0.000030   system_time: 0.000020   total_time: 0.000010   real_time: 0.000300  \n"
+        eq "context:CoordinatorContext user_time:0.000030 system_time:0.000020 total_time:0.000010 real_time:0.000300\n"
     end
 
     it "writes the benchmark result to the log for an ActionUseCase" do
       subject.coordinator(benchmark_result, "UseCaseContext")
       expect(benchmark_log.string).to\
-        eq "context:   UseCaseContext                                     user_time: 0.000030   system_time: 0.000020   total_time: 0.000010   real_time: 0.000300  \n"
+        eq "context:UseCaseContext user_time:0.000030 system_time:0.000020 total_time:0.000010 real_time:0.000300\n"
     end
 
     it "writes the benchmark result to the log for an ActionTask" do
       subject.coordinator(benchmark_result, "TaskContext")
       expect(benchmark_log.string).to\
-        eq "context:   TaskContext                                        user_time: 0.000030   system_time: 0.000020   total_time: 0.000010   real_time: 0.000300  \n"
+        eq "context:TaskContext user_time:0.000030 system_time:0.000020 total_time:0.000010 real_time:0.000300\n"
     end
   end
 end
