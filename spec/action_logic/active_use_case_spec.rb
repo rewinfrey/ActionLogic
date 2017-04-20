1b3e54c8b43fdd4c821f98ece73169146e5561ab
Rick Winfrey <rewinfrey@github.com>
2017-04-19 18:43:09 -0700

    Update specs (wii)

--- spec/action_logic/active_use_case_spec.rb
+++ spec/action_logic/active_use_case_spec.rb
@@ -42,135 +42,153 @@ module ActionLogic
       end
     end
 
-    describe "around validations" do
+    describe "validations" do
       describe "required attributes and type validation" do
-        it "does not raise error if context has required keys and values are of the correct type" do
-          expect { ValidateAroundTestUseCase.execute(Constants::VALID_ATTRIBUTES) }.to_not raise_error
+        describe "with validation! methods" do
+          it "does not raise error if context has required keys and values are of the correct type" do
+            expect { ValidateAroundTestUseCaseWithBang.execute(Constants::VALID_ATTRIBUTES) }.to_not raise_error
+
+            expect { ValidateBeforeTestUseCaseWithBang.execute(Constants::VALID_ATTRIBUTES) }.to_not raise_error
+
+            expect { ValidateAfterTestUseCaseWithBang.execute() }.to_not raise_error
+          end
+
+          it "raises error if context is missing required keys" do
+            expect { ValidateAroundTestUseCaseWithBang.execute() }.to\
+              raise_error(ActionLogic::MissingAttributeError)
+
+            expect { ValidateBeforeTestUseCaseWithBang.execute() }.to\
+              raise_error(ActionLogic::MissingAttributeError)
+
+            expect { ValidateAfterMissingAttributesTestUseCaseWithBang.execute() }.to\
+              raise_error(ActionLogic::MissingAttributeError)
+          end
+
+          it "raises error if context has required keys but values are not of correct type" do
+            expect { ValidateAroundTestUseCaseWithBang.execute(Constants::INVALID_ATTRIBUTES) }.to\
+              raise_error(ActionLogic::AttributeTypeError)
+
+            expect { ValidateBeforeTestUseCaseWithBang.execute(Constants::INVALID_ATTRIBUTES) }.to\
+              raise_error(ActionLogic::AttributeTypeError)
+
+            expect { ValidateAfterInvalidTypeTestUseCaseWithBang.execute() }.to\
+              raise_error(ActionLogic::AttributeTypeError)
+          end
         end
 
-        it "raises error if context is missing required keys" do
-          expect { ValidateAroundTestUseCase.execute() }.to\
-            raise_error(ActionLogic::MissingAttributeError)
-        end
+        describe "with validation methods" do
+          xit "does not record error messages if context has required keys and values are of the correct type" do
+            expect(ValidateAroundTestUseCase.execute(Constants::VALID_ATTRIBUTES).errors.messages).to eq({})
+          end
 
-        it "raises error if context has required keys but values are not of correct type" do
-          expect { ValidateAroundTestUseCase.execute(Constants::INVALID_ATTRIBUTES) }.to\
-            raise_error(ActionLogic::AttributeTypeError)
+          xit "records error messages if context is missing required keys" do
+            expect(ValidateAroundTestUseCase.execute().errors.messages).to eq({:integer_test=>"Value expected to be of type Fixnum but is NilClass", :float_test=>"Value expected to be of type Float but is NilClass", :string_test=>"Value expected to be of type String but is NilClass", :bool_test=>"Value expected to be of type TrueClass but is NilClass", :hash_test=>"Value expected to be of type Hash but is NilClass", :array_test=>"Value expected to be of type Array but is NilClass", :symbol_test=>"Value expected to be of type Symbol but is NilClass", :nil_test=>"nil_test is missing"})
+          end
+
+          xit "records error messages if context has required keys but values are not of correct type" do
+            expect(ValidateAroundTestUseCase.execute(Constants::INVALID_ATTRIBUTES).errors.messages).to eq({:integer_test=>"Value expected to be of type Fixnum but is NilClass", :float_test=>"Value expected to be of type Float but is NilClass", :string_test=>"Value expected to be of type String but is NilClass", :bool_test=>"Value expected to be of type TrueClass but is NilClass", :hash_test=>"Value expected to be of type Hash but is NilClass", :array_test=>"Value expected to be of type Array but is NilClass", :symbol_test=>"Value expected to be of type Symbol but is NilClass", :nil_test=>"Value expected to be of type NilClass but is Fixnum"})
+          end
         end
       end
 
       describe "custom types" do
-        it "allows validation against custom defined types" do
-          expect { ValidateAroundCustomTypeTestUseCase.execute(:custom_type => CustomType1.new) }.to_not raise_error
+        describe "with validation! methods" do
+          it "does not raise error if context has required keys and values match against custom defined types" do
+            expect { ValidateAroundCustomTypeTestUseCaseWithBang.execute(:custom_type => CustomType1.new) }.to_not raise_error
+            expect { ValidateBeforeCustomTypeTestUseCaseWithBang.execute(Constants::CUSTOM_TYPE_ATTRIBUTES1) }.to_not raise_error
+          end
+
+          it "raises error if context has custom defined type attribute but value is not of the correct type" do
+            expect { ValidateAroundCustomTypeTestUseCaseWithBang.execute(:custom_type => CustomType2.new) }.to\
+              raise_error(ActionLogic::AttributeTypeError)
+
+            expect { ValidateBeforeCustomTypeTestUseCaseWithBang.execute(Constants::CUSTOM_TYPE_ATTRIBUTES2) }.to\
+              raise_error(ActionLogic::AttributeTypeError)
+          end
         end
 
-        it "raises error if context has custom type attribute but value is not correct custom type" do
-          expect { ValidateAroundCustomTypeTestUseCase.execute(:custom_type => CustomType2.new) }.to\
-            raise_error(ActionLogic::AttributeTypeError)
-        end
-      end
+        describe "with validation methods" do
+          xit "does not record error messages if context has required keys and values match against custom defined types" do
+            expect(ValidateAroundCustomTypeTestUseCase.execute(:custom_type => CustomType1.new).errors.messages).to eq({})
+          end
 
-      describe "presence" do
-        it "validates presence if presence is specified" do
-          expect { ValidateAroundPresenceTestUseCase.execute(:integer_test => 1) }.to_not raise_error
-        end
-
-        it "raises error if context has required key but value is not defined when validation requires presence" do
-          expect { ValidateAroundPresenceTestUseCase.execute(:integer_test => nil) }.to\
-            raise_error(ActionLogic::PresenceError)
-        end
-      end
-
-      describe "custom presence" do
-        it "allows custom presence validation if custom presence is defined" do
-          expect { ValidateAroundCustomPresenceTestUseCase.execute(:array_test => [1]) }.to_not raise_error
-        end
-
-        it "raises error if custom presence validation is not satisfied" do
-          expect { ValidateAroundCustomPresenceTestUseCase.execute(:array_test => []) }.to\
-            raise_error(ActionLogic::PresenceError)
-        end
-
-        it "raises error if custom presence validation is not supported" do
-          expect { ValidateAroundUnrecognizablePresenceTestUseCase.execute(:integer_test => 1) }.to\
-            raise_error(ActionLogic::UnrecognizablePresenceValidatorError)
-        end
-      end
-    end
-
-    describe "before validations" do
-      describe "required attributes and type validation" do
-        it "does not raise error if context has required keys and values are of the correct type" do
-          expect { ValidateBeforeTestUseCase.execute(Constants::VALID_ATTRIBUTES) }.to_not raise_error
-        end
-
-        it "raises error if context is missing required keys" do
-          expect { ValidateBeforeTestUseCase.execute() }.to\
-            raise_error(ActionLogic::MissingAttributeError)
-        end
-
-        it "raises error if context has required key but is not of correct type" do
-          expect { ValidateBeforeTestUseCase.execute(Constants::INVALID_ATTRIBUTES) }.to\
-            raise_error(ActionLogic::AttributeTypeError)
-        end
-      end
-
-      describe "custom types" do
-        it "allows validation against custom defined types" do
-          expect { ValidateBeforeCustomTypeTestUseCase.execute(Constants::CUSTOM_TYPE_ATTRIBUTES1) }.to_not raise_error
-        end
-
-        it "raises error if context has custom type attribute but value is not correct custom type" do
-          expect { ValidateBeforeCustomTypeTestUseCase.execute(Constants::CUSTOM_TYPE_ATTRIBUTES2) }.to\
-            raise_error(ActionLogic::AttributeTypeError)
+          xit "records error messages if context has custom type attribute but value is not correct custom type" do
+            expect(ValidateAroundCustomTypeTestUseCase.execute(:custom_type => CustomType2.new).errors.messages).to eq({:custom_type=>"Value expected to be of type CustomType1 but is CustomType2"})
+          end
         end
       end
 
       describe "presence" do
-        it "validates presence if presence is specified" do
-          expect { ValidateBeforePresenceTestUseCase.execute(:integer_test => 1) }.to_not raise_error
+        describe "with validation! methods" do
+          it "validates presence if presence is specified" do
+            expect { ValidateAroundPresenceTestUseCaseWithBang.execute(:integer_test => 1) }.to_not raise_error
+            expect { ValidateBeforePresenceTestUseCase.execute(:integer_test => 1) }.to_not raise_error
+          end
+
+          it "raises error if context has required key but value is not defined when validation requires presence" do
+            expect { ValidateAroundPresenceTestUseCaseWithBang.execute(:integer_test => nil) }.to\
+              raise_error(ActionLogic::PresenceError)
+
+            expect { ValidateBeforePresenceTestUseCase.execute(:integer_test => nil) }.to\
+              raise_error(ActionLogic::PresenceError)
+          end
         end
 
-        it "raises error if context has required key but value is not defined when validation requires presence" do
-          expect { ValidateBeforePresenceTestUseCase.execute(:integer_test => nil) }.to\
-            raise_error(ActionLogic::PresenceError)
+        describe "with validation methods" do
+          xit "validates presence if presence is specified" do
+            expect { ValidateAroundPresenceTestUseCase.execute(:integer_test => 1) }.to_not raise_error
+          end
+
+          xit "raises error if context has required key but value is not defined when validation requires presence" do
+            expect { ValidateAroundPresenceTestUseCase.execute(:integer_test => nil) }.to\
+              raise_error(ActionLogic::PresenceError)
+          end
         end
       end
 
       describe "custom presence" do
-        it "allows custom presence validation if custom presence is defined" do
-          expect { ValidateBeforeCustomPresenceTestUseCase.execute(:array_test => [1]) }.to_not raise_error
+        describe "with validation! methods" do
+          it "allows custom presence validation if custom presence is defined" do
+            expect { ValidateAroundCustomPresenceTestUseCaseWithBang.execute(:array_test => [1]) }.to_not raise_error
+            expect { ValidateBeforeCustomPresenceTestUseCaseWithBang.execute(:array_test => [1]) }.to_not raise_error
+          end
+
+          it "raises error if custom presence validation is not satisfied" do
+            expect { ValidateAroundCustomPresenceTestUseCaseWithBang.execute(:array_test => []) }.to\
+              raise_error(ActionLogic::PresenceError)
+
+            expect { ValidateBeforeCustomPresenceTestUseCaseWithBang.execute(:array_test => []) }.to\
+              raise_error(ActionLogic::PresenceError)
+          end
+
+          it "raises error if custom presence validation is not supported" do
+            expect { ValidateAroundUnrecognizablePresenceTestUseCaseWithBang.execute(:integer_test => 1) }.to\
+              raise_error(ActionLogic::UnrecognizablePresenceValidatorError)
+
+            expect { ValidateBeforeUnrecognizablePresenceTestUseCaseWithBang.execute(:integer_test => 1) }.to\
+              raise_error(ActionLogic::UnrecognizablePresenceValidatorError)
+          end
         end
 
-        it "raises error if custom presence validation is not satisfied" do
-          expect { ValidateBeforeCustomPresenceTestUseCase.execute(:array_test => []) }.to\
-            raise_error(ActionLogic::PresenceError)
-        end
+        describe "with validation methods" do
+          xit "allows custom presence validation if custom presence is defined" do
+            expect { ValidateAroundCustomPresenceTestUseCase.execute(:array_test => [1]) }.to_not raise_error
+          end
 
-        it "raises error if custom presence validation is not supported" do
-          expect { ValidateBeforeUnrecognizablePresenceTestUseCase.execute(:integer_test => 1) }.to\
-            raise_error(ActionLogic::UnrecognizablePresenceValidatorError)
+          xit "raises error if custom presence validation is not satisfied" do
+            expect { ValidateAroundCustomPresenceTestUseCase.execute(:array_test => []) }.to\
+              raise_error(ActionLogic::PresenceError)
+          end
+
+          xit "raises error if custom presence validation is not supported" do
+            expect { ValidateAroundUnrecognizablePresenceTestUseCase.execute(:integer_test => 1) }.to\
+              raise_error(ActionLogic::UnrecognizablePresenceValidatorError)
+          end
         end
       end
     end
 
     describe "after validations" do
-      describe "required attributes and type validation" do
-        it "does not raise error if the task sets all required keys and values are of the correct type" do
-          expect { ValidateAfterTestUseCase.execute() }.to_not raise_error
-        end
-
-        it "raises error if task does not provide the necessary keys" do
-          expect { ValidateAfterMissingAttributesTestUseCase.execute() }.to\
-            raise_error(ActionLogic::MissingAttributeError)
-        end
-
-        it "raises error if task has required key but is not of correct type" do
-          expect { ValidateAfterInvalidTypeTestUseCase.execute() }.to\
-            raise_error(ActionLogic::AttributeTypeError)
-        end
-      end
-
       describe "custom types" do
         it "allows validation against custom defined types" do
           expect { ValidateAfterCustomTypeTestUseCase.execute() }.to_not raise_error
